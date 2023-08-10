02eb51a2d6c429b96cec119dc0d898b3b5125cee
zixsorus <touy_elec@hotmail.com>
2023-08-10 16:28:50 +0700

    Update Search

--- utils/index.ts
+++ utils/index.ts
@@ -7,7 +7,7 @@ export async function fetchCar( filters: FilterProps) {
         'X-RapidAPI-Key': '058bbea680msh03b297ea12b27b6p1397fajsn8a6a350cf5a0',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
-    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}$year=${year}&limit=${limit}&fule=${fuel}&model=${model}`, {headers: headers});
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&limit=${limit}&fule=${fuel}&model=${model}`, {headers: headers});
 
     const result = await response.json();
 
@@ -15,7 +15,7 @@ export async function fetchCar( filters: FilterProps) {
 }
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
-    const basePricePerDay = 20; // Base rental price per day in dollars
+    const basePricePerDay = 30; // Base rental price per day in dollars
     const mileageFactor = 0.16; // Additional rate per mile driven
     const ageFactor = 0.05; // Additional rate per year of vehicle age
     const conversionRate = 34; // Conversion rate from USD to THB (1 USD = 34 THB)
@@ -54,4 +54,14 @@ export const generateCarImageUrl = (car: CarProps, angle?:string) => {
     url.searchParams.append('angle', `${angle}`);
 
     return `${url}`
+}
+
+export const updateSearchParams = (type : string, value : string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    searchParams.set(type, value);
+
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`
+
+    return newPathname;
 }
